dd0051b8e27a44f676e578726605f8dac3b3f659
Stephan Kugler (WSL) <stephankugler.stuff@gmail.com>
2024-11-11 13:54:34 +0100

    add cleanup function to getMovies function, and handle race condition

--- src/App.jsx
+++ src/App.jsx
@@ -86,12 +86,15 @@ export default function App() {
 
   useEffect(
     function () {
+      const controller = new AbortController();
+
       async function getMovies() {
         try {
           setIsLoading(true);
           setError('');
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            { signal: controller.signal }
           );
 
           if (!res.ok)
@@ -100,8 +103,11 @@ export default function App() {
           const data = await res.json();
           if (data.Response === 'False') throw new Error('Movie not found');
           setMovies(data.Search);
+          setError('');
         } catch (err) {
-          setError(err.message);
+          if (err.name !== 'AbortError') {
+            setError(err.message);
+          }
         } finally {
           setIsLoading(false);
         }
@@ -114,6 +120,10 @@ export default function App() {
       }
 
       getMovies();
+
+      return function () {
+        controller.abort();
+      };
     },
     [query]
   );
