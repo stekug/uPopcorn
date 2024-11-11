ee78b0bafef2fabb7959ce0b1fe2e3fa6539f409
Stephan Kugler (WSL) <stephankugler.stuff@gmail.com>
2024-11-11 14:08:28 +0100

    add escape key keyprass function to close a movie

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -76,6 +76,23 @@ export default function MovieDetails({
 
   useEffect(
     function () {
+      function callback(e) {
+        if (e.code === 'Escape') {
+          onCloseMovie();
+        }
+      }
+
+      document.addEventListener('keydown', callback);
+
+      return function () {
+        document.removeEventListener('keydown', callback);
+      };
+    },
+    [onCloseMovie]
+  );
+
+  useEffect(
+    function () {
       if (!title) return;
       document.title = `Movie | ${title}`;
 
