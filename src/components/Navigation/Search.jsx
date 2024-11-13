9f2bf39ced929f52dda732f12ecd0ddac7776248
Stephan Kugler (WSL) <stephankugler.stuff@gmail.com>
2024-11-13 09:37:31 +0100

    build useKey hook and use it in MovieDetails and Search

--- src/components/Navigation/Search.jsx
+++ src/components/Navigation/Search.jsx
@@ -1,24 +1,14 @@
-import { useEffect, useRef } from 'react';
+import { useRef } from 'react';
+import { useKey } from '../useKey';
 
 export default function Search({ query, setQuery }) {
   const inputElement = useRef(null);
 
-  useEffect(
-    function () {
-      function callback(event) {
-        if (document.activeElement === inputElement.current) return;
-
-        if (event.code === 'Enter') {
-          inputElement.current.focus();
-          setQuery('');
-        }
-      }
-
-      document.addEventListener('keydown', callback);
-      return () => document.addEventListener('keydown', callback);
-    },
-    [setQuery]
-  );
+  useKey('Enter', function () {
+    if (document.activeElement === inputElement.current) return;
+    inputElement.current.focus();
+    setQuery('');
+  });
 
   return (
     <input
